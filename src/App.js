fe8ba955d300c3d8b2d53222fff1ab5e366828cf
mehedih23 <nmehedi23@gmail.com>
2022-05-19 18:33:44 +0600

    Home banner done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import banner from '../../assets/images/image 1.png'
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div class="hero h-4/5 bg-base-100">
+            <div class="hero-content flex-col lg:flex-row-reverse w-full">
+                <div>
+                    <img src={banner} className="max-w-sm rounded-lg" alt='banner' />
+                </div>
+                <div>
+                    <h1 class="text-4xl font-bold">
+                        Join the IT industry in <br />
+                        Africa in non-coding <br />
+                        tech roles
+                    </h1>
+                    <p class="py-6">
+                        Build non-coding tech skills for the IT industry <br />
+                        Get ready for an intern level role
+                    </p>
+                    <button class="btn bg-gradient-to-r from-[#13DE82] to-[#0AB68B] border-0 text-neutral rounded-full">Get Started</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
